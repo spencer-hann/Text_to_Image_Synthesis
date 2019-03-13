2f2fb5e799409eda7662da3952f977dc57ef2b55
jeff-lund <jdl3@pdx.edu>
2019-03-13 08:45:08 -0700

    working gan-cls model

--- gan.py
+++ gan.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 import argparse
 import os
+import time
 import random
 import torch
 import torch.nn as nn
@@ -14,18 +15,19 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
 from dataset2 import TTI_Dataset
+from models import Concat, Discriminator, Generator
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', default='folder' , help='cifar10 | lsun | mnist |imagenet | folder | lfw | fake')
 parser.add_argument('--dataroot', default='.', help='path to dataset')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
-parser.add_argument('--batchSize', type=int, default=32, help='input batch size')
+parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
-parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
+parser.add_argument('--niter', type=int, default=100, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
@@ -35,24 +37,6 @@ parser.add_argument('--netD', default='', help="path to netD (to continue traini
 parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
 parser.add_argument('--manualSeed', type=int, help='manual seed')
 
-
-class Concat(nn.Module):
-
-    def __init__(self, embed_dim, projected_embed_dim):
-        super(Concat, self).__init__()
-        self.projection = nn.Sequential(
-            nn.Linear(in_features=embed_dim, out_features=projected_embed_dim),
-            #nn.BatchNorm1d(num_features=projected_embed_dim),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True)
-            )
-
-    def forward(self, inp, embed):
-        projected_embed = self.projection(embed)
-        replicated_embed = projected_embed.repeat(4, 4, 1, 1).permute(2,  3, 0, 1)
-        hidden_concat = torch.cat([inp, replicated_embed], 1)
-
-        return hidden_concat
-
 # custom weights initialization called on netG and netD
 def weights_init(m):
     classname = m.__class__.__name__
@@ -61,96 +45,6 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
-
-
-class Generator(nn.Module):
-    def __init__(self, ngpu):
-        super(Generator, self).__init__()
-        self.ngpu = ngpu
-        self.noise_dim = 100
-        self.embed_dim = 1024
-        self.projected_dim = 128
-        self.nc = 3
-        self.ngf = 64
-        self.latent_dim = self.noise_dim + self.projected_dim
-
-        self.projection = nn.Sequential(
-            nn.Linear(in_features=self.embed_dim, out_features=self.projected_dim),
-            #nn.BatchNorm1d(num_features=self.projected_dim),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True)
-        )
-
-        self.main = nn.Sequential(
-            # input is Z, going into a convolution
-            nn.ConvTranspose2d(self.latent_dim, self.ngf * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(self.ngf * 8),
-            nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
-            nn.ConvTranspose2d(self.ngf * 8, self.ngf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(self.ngf * 4),
-            nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
-            nn.ConvTranspose2d(self.ngf * 4, self.ngf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(self.ngf * 2),
-            nn.ReLU(True),
-            # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d(self.ngf * 2,     self.ngf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(self.ngf),
-            nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
-            nn.ConvTranspose2d(    self.ngf,      self.nc, 4, 2, 1, bias=False),
-            nn.Tanh()
-            # state size. (nc) x 64 x 64
-        )
-
-    def forward(self, txt, z):
-        projected_embed = self.projection(txt).unsqueeze(2).unsqueeze(3)
-        input = torch.cat([projected_embed, z], 1)
-        output = self.main(input)
-
-        return output
-
-class Discriminator(nn.Module):
-    def __init__(self, ngpu):
-        super(Discriminator, self).__init__()
-        self.ngpu = ngpu
-        self.embed_dim = 1024
-        self.projected_dim = 128
-        self.nc = 3
-        self.ndf = 64
-
-        self.D1 = nn.Sequential(
-            # input is (nc) x 64 x 64
-            nn.Conv2d(self.nc, self.ndf, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf) x 32 x 32
-            nn.Conv2d(self.ndf, self.ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(self.ndf * 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
-            nn.Conv2d(self.ndf * 2, self.ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(self.ndf * 4, self.ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(self.ndf * 8),
-            nn.LeakyReLU(0.2, inplace=True),
-        )
-        self.projector = Concat(self.embed_dim, self.projected_dim)
-
-        self.D2 = nn.Sequential(
-            # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(self.ndf * 8 + self.projected_dim, 1, 4, 1, 0, bias=False),
-            nn.Sigmoid()
-        )
-
-    def forward(self, img, txt):
-        intermediate = self.D1(img)
-        output = self.projector(intermediate, txt)
-        output = self.D2(output)
-
-        return output.view(-1, 1).squeeze(1)
-
 # START
 
 opt = parser.parse_args()
@@ -208,47 +102,44 @@ fake_label = 0
 optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 print('starting epochs')
+starting_time = time.time()
 for epoch in range(opt.niter):
+    etime = time.time()
     # right image, right embed, wrong embed
-    for i, (ri, re, we) in enumerate(dataloader, 0):
+    for i, (real_image, real_embedding, wrong_embedding) in enumerate(dataloader, 0):
         if opt.cuda:
-            ri = ri.to(device)
-            re = re.to(device)
-            we = we.to(device)
-
-        # steps from Algorithm 1 from paper
-        #5. get noise vector z
-        #6 fi = forward G(z, re)
-        #7 s_r = D(ri, re)
-        #8 s_w D(ri, we)
-        #9 s_f D(fi, re)
-        #10 Calculate L_D = log(s_r) + (log(1-s_w) + log(1-s_f))/2
-        #11 Update D
-        #12 Calculate L_G = log(s_f)s
-        #13 Update G
+            real_image = real_image.to(device)
+            real_embedding = real_embedding.to(device)
+            wrong_embedding = wrong_embedding.to(device)
 
         ############################
         # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
         ###########################
-        # train with real
+        # train with real image, real embed
         netD.zero_grad()
 
-        batch_size = ri.size(0)
+        batch_size = real_image.size(0)
         label = torch.full((batch_size,), real_label, device=device)
-        output = netD(ri, re)
+        output = netD(real_image, real_embedding)
         errD_real = criterion(output, label)
         errD_real.backward()
         D_x = output.mean().item()
 
+        # cls real img, fake embed
+        label.fill_(fake_label)
+        output2 = netD(real_image, wrong_embedding)
+        errD_wrong = criterion(output2, label)
+        errD_wrong.backward()
         # train with fake
         noise = torch.randn(batch_size, nz, 1, 1, device=device)
-        fake = netG(re, noise)
+        fake = netG(real_embedding, noise)
         label.fill_(fake_label)
-        output = netD(fake.detach(), re)
+        output = netD(fake.detach(), real_embedding)
         errD_fake = criterion(output, label)
         errD_fake.backward()
+        errD = errD_real + errD_fake + errD_wrong
+
         D_G_z1 = output.mean().item()
-        errD = errD_real + errD_fake
         optimizerD.step()
 
         ############################
@@ -256,24 +147,28 @@ for epoch in range(opt.niter):
         ###########################
         netG.zero_grad()
         label.fill_(real_label)  # fake labels are real for generator cost
-        output = netD(fake, re)
+        output = netD(fake, real_embedding)
         errG = criterion(output, label)
         errG.backward()
         D_G_z2 = output.mean().item()
         optimizerG.step()
 
+
+
         print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
               % (epoch, opt.niter, i, len(dataloader),
                  errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
         if i % 100 == 0:
-            vutils.save_image(ri,
+            vutils.save_image(real_image,
                     '%s/real_samples.png' % opt.outf,
                     normalize=True)
-            fake = netG(re ,fixed_noise)
+            fake = netG(real_embedding ,fixed_noise)
             vutils.save_image(fake.detach(),
                     '%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),
                     normalize=True)
-
+    print("Epoch time:", time.time() - etime)
     # do checkpointing
     torch.save(netG.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
     torch.save(netD.state_dict(), '%s/netD_epoch_%d.pth' % (opt.outf, epoch))
+ending_time = time.time()
+print("Total runtime for epochs", ending_time - starting_time)
